acb0bfa69b87d04bcd2ab4853085807ea9f4ade1
Evans Ding <evansding@gmail.com>
2022-04-12 18:41:47 -0700

    security changes

--- playlist-generator/src/utils/SpotifyApi.js
+++ playlist-generator/src/utils/SpotifyApi.js
@@ -1,10 +1,12 @@
 /* eslint-disable camelcase */
 
+import id from './clientID.js';
+
 /**
  * You must create a Spotify App here https://developer.spotify.com/dashboard/applications and replace this CLIENT_ID with your App's CLIENT_ID.
  * Also, you must add 'http://localhost:3000/callback' as one of your app's 'Redirect URIs'
  */
-export const CLIENT_ID = '58d90499a42f4cfc88c7dbd61a79b00a';
+export const CLIENT_ID = id;
 export const REDIRECT_URI = 'http://localhost:3000/callback';
 
 export const LOGIN_URL = 'https://accounts.spotify.com/authorize';
---
+++ playlist-generator/src/utils/clientID.js
@@ -0,0 +1,3 @@
+const Id = '58d90499a42f4cfc88c7dbd61a79b00a';
+
+export default Id;
